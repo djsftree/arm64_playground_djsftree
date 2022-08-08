5dc704f0f129d50a63aa080d84ccfe8239925c0d
djsftree <10553134+djsftree@users.noreply.github.com>
2022-08-08 18:02:16 -0400

    Update make_debs.sh

--- make_debs.sh
+++ make_debs.sh
@@ -5,8 +5,12 @@ DEBFULLNAME="djsftree"
 export DEBEMAIL DEBFULLNAME
 export DEBUILD_DPKG_BUILDPACKAGE_OPTS="-d"
 
+
+
 apt install -y qtbase5-private-dev libqt5svg5-dev qttools5-dev libqt5x11extras5-dev libpolkit-qt5-1-dev
+
 apt install -y libkf5guiaddons-dev libkf5idletime-dev libkf5screen-dev libkf5windowsystem-dev libkf5solid-dev
+
 apt install -y bash libstatgrab-dev libudev-dev libasound2-dev libpulse-dev libsensors4-dev libconfig-dev \
   libmuparser-dev libupower-glib-dev libpolkit-agent-1-dev libpolkit-qt5-1-dev sudo libexif-dev x11-utils \
   libxss-dev libxcursor-dev libxcomposite-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev \
@@ -22,6 +26,7 @@ apt source -y lxqt-build-tools libqtxdg liblxqt libsysstat qtxdg-tools \
   lxqt-runner lxqt-archiver lximage-qt qtermwidget qterminal qps screengrab
 
 
+
 cd lxqt-build-tools-0.11.0
 dch -a "compiled by djsftree"
 debuild -i -us -uc -b
@@ -29,6 +34,7 @@ cd ..
 rm *dbgsym*.deb
 rm *l10n*.deb
 dpkg -i lxqt-build-tools*.deb
+
 
 
 cd libqtxdg-3.9.1
@@ -41,6 +47,7 @@ dpkg -i libqt5xdg*.deb
 dpkg -i qtxdg-dev-tools*.deb
 
 
+
 cd qtxdg-tools-3.9.1
 dch -a "compiled by djsftree"
 debuild -i -us -uc -b
@@ -48,6 +55,7 @@ cd ..
 rm *dbgsym*.deb
 rm *l10n*.deb
 dpkg -i qtxdg-tools*.deb
+
 
 
 cd liblxqt-1.1.0
@@ -63,6 +71,7 @@ dpkg -r --force-depends liblxqt-globalkeys0
 dpkg -i liblxqt*.deb
 
 
+
 cd libsysstat-0.4.6
 dch -a "compiled by djsftree"
 debuild -i -us -uc -b
@@ -70,6 +79,7 @@ cd ..
 rm *dbgsym*.deb
 rm *l10n*.deb
 dpkg -i libsysstat*.deb
+
 
 
 cd libfm-qt-1.1.0
@@ -102,6 +112,7 @@ do
 done
 
 
+
 dpkg -r --force-depends libqtermwidget5-0
 dpkg -i lxqt-themes*.deb
 dpkg -i lxqt-system-theme*.deb
@@ -113,6 +124,8 @@ dpkg -i qtermwidget*.deb
 
 git clone --branch debian/experimental https://salsa.debian.org/lxqt-team/lxqt-qtplugin.git lxqt-qtplugin-1.1.0
 
+
+
 CMAKE_REPOS=" \
     lxqt-about-1.1.0 \
     lxqt-admin-1.1.0 \
@@ -121,8 +134,38 @@ CMAKE_REPOS=" \
     lxqt-notificationd-1.1.0 \
     lxqt-openssh-askpass-1.1.0 \
     lxqt-policykit-1.1.0 \
+    lxqt-qtplugin-1.1.0"
+    
+    
+    
+for package in ${CMAKE_REPOS}
+do
+    echo "\n\n Building: $package\n"
+    cd $package
+    dch -a "compiled by djsftree"
+    debuild -i -us -uc -b
+    cd ..
+    rm *dbgsym*.deb
+    rm *l10n*.deb
+done
+
+
+
+dpkg -i lxqt-about*.deb
+dpkg -i lxqt-admin*.deb
+dpkg -i lxqt-config*.deb
+dpkg -i liblxqt-globalkeys*.deb
+dpkg -i lxqt-globalkeys*.deb
+dpkg -i lxqt-notificationd*.deb
+dpkg -i lxqt-openssh-askpass*.deb
+dpkg -i lxqt-qtplugin*.deb
+dpkg -i lxqt-session*.deb
+dpkg -i lxqt-policykit*.deb
+
+
+
+CMAKE_REPOS=" \
     lxqt-powermanagement-1.1.0 \
-    lxqt-qtplugin-1.1.0 \
     lxqt-session-1.1.1 \
     lxqt-sudo-1.1.0 \
     pcmanfm-qt-1.1.0 \
@@ -133,6 +176,8 @@ CMAKE_REPOS=" \
     qterminal-1.1.0 \
     qps-2.5.0 \
     screengrab-2.4.0"
+
+
 
 for package in ${CMAKE_REPOS}
 do
@@ -145,17 +190,10 @@ do
     rm *l10n*.deb
 done
 
-dpkg -i lxqt-about*.deb
-dpkg -i lxqt-admin*.deb
-dpkg -i lxqt-config*.deb
-dpkg -i liblxqt-globalkeys*.deb
-dpkg -i lxqt-globalkeys*.deb
-dpkg -i lxqt-notificationd*.deb
-dpkg -i lxqt-openssh-askpass*.deb
-dpkg -i lxqt-session*.deb
-dpkg -i lxqt-policykit*.deb
+
+
+
 dpkg -i lxqt-powermanagement*.deb
-dpkg -i lxqt-qtplugin*.deb
 dpkg -i lxqt-sudo*.deb
 dpkg -i pcmanfm-qt*.deb
 dpkg -i lxqt-panel*.deb
